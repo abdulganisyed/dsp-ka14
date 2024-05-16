977c701b5788fc43640b34fa3d881634ee37bd4c
abdulganisyed <softwareengineering301@gmail.com>
2024-05-16 08:42:40 +0530

    Add files via upload

---
+++ Package my pack.java
@@ -0,0 +1,22 @@
+Package my pack;
+class employee
+{
+int id;string name;float salary;
+void insert iny i,string n, flost s){
+void display()
+system.out.println("id+""+salary);
+public class test employee
+{
+public static void main (string[]args){
+empolyee e1=new employee();
+empolyee e2=new employee();
+empolyee e3=new employee();
+e1.insert(101,"sayed",25000);
+e2.insert(102,"muzzu",35000);
+e3.insert(103,muzammil",45000);
+e1.display();
+e2.display();
+e3.display();
+}
+}
+}
